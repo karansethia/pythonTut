9b93fc7a9896ed9692190f645fa349a1469b94ff
Karan <karansethia24@gmail.com>
2022-03-04 15:14:06 +0530

    Learning Python

---
+++ main.py
@@ -0,0 +1,23 @@
+# Hello world program
+print("Hello world")
+
+# Printing multiline string
+print('''My name is Karan
+I am a front End Developer 
+I am the Dragon Emperor''')
+
+#Printing data type of variable
+a = "Karan"
+b = 2
+c = 3.14
+d = True
+e = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+a=8
+a-=2
+print(a)
